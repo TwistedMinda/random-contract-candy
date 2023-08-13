f47fdc160f85b2e364d2c419a35fd5420b0b1246
Julien <julien@goliaths.io>
2023-08-13 22:19:51 +0200

    clean up deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,10 +2,7 @@ import { ethers } from "hardhat";
 import { deployCandy } from "../artifacts/contracts/src/tools";
 
 async function main() {
-  const network = 'sepolia'
-  const randomizer = await deployCandy(network)
-  const address = await randomizer.getAddress()
-  console.log('> Now add address in config: ', address, 'for network', network)
+  await deployCandy('sepolia')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
